203909a6a465ae628f13c2982b69db1788c20af2
AMinSC <https://github.com/AMiSC/TIL.git>
2022-05-13 23:39:44 +0900

    feat: Baekjoon # 10951

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,5 +1,17 @@
+# 10952
 T = int(input())
 
 for i in range(T):
     a, b = map(int, input().split())
     print(a+b)
+
+
+# 10951
+while True:
+    try:
+        a, b = map(int, input().split())
+        print(a+b)
+    except:
+        break
+
+
