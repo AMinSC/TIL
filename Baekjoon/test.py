fa35918e34f7736aa429b58694a9406001992066
AMinSC <https://github.com/AMiSC/TIL.git>
2022-05-15 21:44:53 +0900

    feat: Baekjoon # 1110
    end of Step 3

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -14,4 +14,20 @@ while True:
     except:
         break
 
+# 1110
+num_input = num = int(input())
 
+cnt = 0
+
+while True:
+    num1 = num // 10
+    num2 = num % 10
+    num_sum = num1 + num2
+
+    num = int(str(num2) + str(num_sum % 10))
+    
+    cnt += 1
+
+    if num_input == num:
+        break
+print(cnt)
