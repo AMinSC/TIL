9d03637a8e9e5002631e85b51a7fc9a8638a73c4
AMinSC <https://github.com/AMinSC/fastcampus_study.git>
2022-06-20 21:22:04 +0900

    kaggle submit

--- PJT_modelling/main.py
+++ PJT_modelling/main.py
@@ -2,24 +2,42 @@
 
 import pandas as pd
 
+from catboost import CatBoostRegressor
+
 from processing import checkNaN, dropNaN
 from featuring import *
 from modelling import fit_model
 
 def main() :
     print("Data loading...")
-    train = pd.read_csv("./data/train_V2.csv")
-    test = pd.read_csv("./data/test_V2.csv")
+    train = pd.read_csv("../input/pubg-finish-placement-prediction/train_V2.csv")
+    test = pd.read_csv("../input/pubg-finish-placement-prediction/test_V2.csv")
     print("Data loaded!")
     
+    # pre-processing train
     checkNaN(train)
     train = dropNaN(train)
-    
     train = dropOutlier(train)
     train = encodeMatch(train)
     train = makeCols(train)
+    train = train.drop(["Id","groupId", "matchType","matchId", "numGroups","damageDealt"], axis=1) 
     
-    train = fit_model(train)
+    # pre-processing test
+    test = encodeMatch(test)
+    test = makeCols(test)
+    test = test.drop(["Id","groupId", "matchType","matchId", "numGroups","damageDealt"], axis=1) 
+    
+    X = train.drop(["winPlacePerc"], axis=1)
+    y = train['winPlacePerc']
+    
+    model = CatBoostRegressor(iterations=40, depth=16, learning_rate=1, loss_function='MAE')
+    model.fit(X, y)
+    
+    result = model.predict(test)
+    
+    sample_submission = pd.read_csv("../input/pubg-finish-placement-prediction/sample_submission_V2.csv",index_col = "Id")
+    sample_submission["winPlacePerc"] = result
+    sample_submission.to_csv("submission.csv")
 
 if __name__=="__main__" :
     main()
--- PJT_modelling/modelling.py
+++ PJT_modelling/modelling.py
@@ -1,4 +1,18 @@
+
+
 # data set split and 4 models training
+import numpy as np
+
+from sklearn.linear_model import LinearRegression as linear
+from sklearn.linear_model import Ridge as ridge
+from sklearn.linear_model import Lasso as lasso
+from xgboost.sklearn import XGBRFRegressor as xgboost
+from lightgbm.sklearn import LGBMRegressor as lgbm
+from catboost import CatBoostRegressor as catboost
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import mean_absolute_error
 
 def fit_model(df) :
     features = df.drop(["Id","groupId", "matchType","matchId", "numGroups","damageDealt","winPlacePerc"], axis=1) 
@@ -6,7 +20,7 @@ def fit_model(df) :
     
     train_X, test_X, train_y, test_y = train_test_split(features, target, test_size=0.2, random_state=589)
     
-    for model_func in [linear, ridge, lasso, lgbm]:
+    for model_func in [linear, ridge, lasso, lgbm, xgboost, catboost]:
       print(f"{model_func} Fitting...")
       model = model_func().fit(train_X, train_y)
       pred_y = model.predict(test_X)
