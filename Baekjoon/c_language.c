0afec49aa502fdae96fa12b18f823d263a929cdb
민성철 <cdtjdcjf@gmail.com>
2023-01-08 17:00:16 +0900

    baekjoon another code, contribution testing

--- Baekjoon/c_language.c
+++ Baekjoon/c_language.c
@@ -8,35 +8,13 @@ int main(void)
 	int a[32];
 	for (int i = 0; i < 28; i++)
 	{
-		scanf("%d", &a[i]);
+		int n;
+		scanf("%d", &n);
+		a[n-1] = 1;
 	}
-	int c=0;
-	while(c < 28)
-	{
-	    for (int i = 0; i < 28; i++)
-		{
-			if (a[i] > a[i+1])
-			{
-				if (1<= a[i+1] & a[i+1] <= 30)
-				{
-					int b = a[i];
-					a[i] = a[i+1];
-					a[i+1] = b;
-				}
-			}
-		}
-		c++;
-	}
-	printf("\n\n");
-	int b[32];
 	for (int i = 0; i < 30; i++)
 	{
-		b[i] = i+1;
-		for (int j = 0; j < 28; j++)
-		{
-			if (b[i] == a[j]) {b[i] = 0;}
-		}
-		if (b[i] > 0) {printf("%d\n", b[i]);}
+		if (a[i] == 0){printf("%d\n", i+1);}
 	}
 	return 0;
 }
