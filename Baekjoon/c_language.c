b2a734740dad17918c69854a76d90ce392297f5b
민성철 <cdtjdcjf@gmail.com>
2023-02-07 15:30:18 +0900

    #8958

--- Baekjoon/c_language.c
+++ Baekjoon/c_language.c
@@ -1,41 +1,61 @@
 // 백준 C언어로 다시 문제 풀기
 // studing..
 
-#include <stdio.h>
+#include 	<stdio.h>
 #include	<stdlib.h>
+
+int	ans_check(char c, char b)
+{
+	if (c == b)
+		return (1);
+	return (0);
+}
+
+int	point_check(char *arr, char c)
+{
+	int	i = 0, total_point = 0, point = 0;
+
+	while (arr[i])
+	{
+		if (ans_check(arr[i], c) && !ans_check(arr[i - 1], c))
+		{
+			point = 1;
+			i++;
+		}
+		else if (ans_check(arr[i], c) && ans_check(arr[i - 1], c))
+		{
+			point++;
+			i++;
+		}
+		while (arr[i] && !ans_check(arr[i], c))
+			i++;
+		total_point += point;
+	}
+	return (total_point);
+}
 
 int main(void)
 {
-	float	*total_exam;
-	float	answer = 0;
-	int	cnt, exam, idx = 0, first_idx = 0, second_idx;
+	char answer[64];
+	char ans = 'O';
+	int	*ans_point;
+	int	total_idx = 0, idx = 0;
 
-	scanf("%d", &cnt);
-	total_exam = (float *)malloc(sizeof(float) * cnt);
-	while (idx < cnt)
+	scanf("%d", &total_idx);
+	ans_point = (int *)malloc(sizeof(int) * total_idx);
+	if (!ans_point)
+		return (0);
+
+	while (idx < total_idx)
 	{
-		scanf("%d", &exam);
-		total_exam[idx] = exam;
+		scanf("%s", answer);
+		ans_point[idx] = point_check(answer, ans); // point_check func
 		idx++;
-	}
-	while (first_idx < cnt - 1)
-	{
-		second_idx = first_idx + 1;
-		if (total_exam[first_idx] > total_exam[second_idx])
-		{
-			int	tmp;
-			tmp = total_exam[first_idx];
-			total_exam[first_idx] = total_exam[second_idx];
-			total_exam[second_idx] = tmp;
-		}
-		first_idx++;
 	}
 	idx = 0;
-	while (idx < cnt)
+	while (idx < total_idx)
 	{
-		total_exam[idx] = total_exam[idx] / total_exam[cnt - 1] * 100;
-		answer += total_exam[idx];
+		printf("%d\n", ans_point[idx]);
 		idx++;
 	}
-	printf("%.4f\n", answer /= cnt); 
 }
