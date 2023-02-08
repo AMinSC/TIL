7a15241c05d448f589d06d7ea762b7ecbad3a08e
민성철 <cdtjdcjf@gmail.com>
2023-02-08 23:08:52 +0900

    #4673

--- Baekjoon/func.c
+++ Baekjoon/func.c
@@ -1,59 +1,50 @@
 #include	<stdio.h>
 
-int	*selp_check(int *arr, int *check, int num)
+int	n(int num)
 {
-	int	idx = 0, selp_idx = 0;
-	while (idx <= num)
-	{
-		selp_idx = 0;
-		while (selp_idx <= num)
-		{
-			if (check[idx] == arr[selp_idx])
-				check[idx] = 0;
-			selp_idx++;
-		}
-		idx++;
-	}
-	return (check);
+	if (9 >= num)
+		num += num;
+	else if (1000 <= num)
+		num = num + ((num / 1000) % 10) + ((num / 100) % 10) + ((num / 10) % 10) + (num %10);
+	else if (100 <= num)
+		num = num + ((num / 100) % 10) + ((num / 10) % 10) + (num % 10);
+	else if (10 <= num)
+		num = num + ((num / 10) % 10) + (num % 10);
+	return (num);
 }
 
-void	selp_num(void)
+int	main(void)
 {
-	int	*answer;
-	int	arr[10000], selp_arr[10000];
-	int	num = 10000, idx = 1;
+	int	arr[10001], selp_arr[10001]; // 10000번째 수까지 적용해야 되서 사이즈를 1 늘려줬다
+	int	num = 10000, idx = 0, sub_idx = 0;
 
 	while (idx <= num)
 	{
+		if (0 < idx)
+			arr[idx] = n(idx);
 		selp_arr[idx] = idx;
 		idx++;
 	}
 	idx = 0;
 	while (idx <= num)
 	{
-		int	selp = 0;
-		if (9 >= idx)
-			selp = idx + idx;
-		else if (1000 <= idx)
-			selp = idx + (idx / 1000) + ((idx / 100) % 10) + ((idx / 10) % 10) + (idx % 10);
-		else if (100 <= idx)
-			selp = idx + (idx / 100) + ((idx / 10) % 10) + (idx % 10);
-		else if (10 <= idx)
-			selp = idx + (idx / 10) + (idx % 10);
-		arr[idx] = selp;
+		sub_idx = 0;
+		while (sub_idx <= num)
+		{
+			if (selp_arr[idx] == arr[sub_idx])
+			{
+				selp_arr[idx] = 0;
+				break ;
+			}
+			sub_idx++;
+		}
 		idx++;
 	}
-	answer = selp_check(arr, selp_arr, num);
 	idx = 0;
 	while (idx <= num)
 	{
-		if (0 < answer[idx])
-			printf("%d\n", answer[idx]);
+		if (0 < selp_arr[idx])
+				printf("%d\n", selp_arr[idx]);
 		idx++;
 	}
-}
-
-int main(void)
-{
-	selp_num();
 }
