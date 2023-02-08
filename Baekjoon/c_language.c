22e1f9fbb7dc4fe9518aaa2171d19551226b06c3
민성철 <cdtjdcjf@gmail.com>
2023-02-08 20:42:59 +0900

    #4344 done, #4673 time out..

--- Baekjoon/c_language.c
+++ Baekjoon/c_language.c
@@ -2,71 +2,41 @@
 // studing..
 
 #include 	<stdio.h>
-#include	<stdlib.h>
-
-// 각 반의 총 합 점수 구하는 함수
-double	score_check(double *arr)
-{
-	int	i = 0;
-	double score = 0;
-
-	while (arr[i]) // 배열을 돌면서 score변수에 더함
-	{
-		score += arr[i];
-		i++;
-	}
-	return (score); // 반환
-}
-
-// 총합의 평균을 초과하는 점수만 count해서 100분율 내서 반환하는 함수
-double	class_check(void)
-{
-	double	*class;
-	double	exam_score, total_score, student = 0, cnt = 0;
-	int	idx = 0;
-
-	scanf("%lf", &student); // 몇 명인지 input으로 받음
-	class = (double *)malloc(sizeof(double) * student);
-	if (!class)
-		return (0);
-
-	while (idx < student) // 각 학생의 점수를 배열에 저장
-	{
-		scanf("%lf", &exam_score);
-		class[idx] = exam_score;
-		idx++;
-	}
-	total_score = score_check(class); // 총점을 구하는 함수
-	total_score /= student; // 총점을 학생수로 나눔 (평균)
-	idx = 0;
-	while (idx < student) // 평균 초과되는 인원 체크
-	{
-		if (total_score < class[idx])
-			cnt++;
-		idx++;
-	}
-	student = 100 / student; // 100분율을 구하기 위해 100을 학생수로 나눔
-	//cnt *= student;
-	return (cnt *= student); // 100으로 나눈 값과 평균을 넘는 학생을 곱해서 반환
-}
 
 int main(void)
 {
-	double	*class;
-	int	class_num = 0, idx = 0;
+	double	total_score;
+	int	class_num, exam_score, student, idx = 0, stud_idx, cnt = 0;
 
-	scanf("%d", &class_num); // 몇 반까지 있는지 input으로 받음
-	class = (double *)malloc(sizeof(double) * class_num);
-	if (!class)
-		return (0);
-
-	while (idx < class_num) // input으로 받은 반까지 반복문을 돌며 100분율 반환값을 배열에 저장
+	scanf("%d", &class_num);
+	double	class[class_num];
+	while (idx < class_num)
 	{
-		class[idx] = class_check(); // class_check func
+		scanf("%d", &student);
+		int	student_score[student];
+		stud_idx = 0;
+		total_score = 0.0;
+		while (stud_idx < student)
+		{
+			scanf("%d", &exam_score);
+			student_score[stud_idx] = exam_score;
+			total_score += exam_score;
+			stud_idx++;
+		}
+		total_score /= student;
+		stud_idx = 0;
+		cnt = 0;
+		while (stud_idx < student)
+		{
+			if (total_score < student_score[stud_idx])
+				cnt++;
+			stud_idx++;
+		}
+		class[idx] = (100 / (double)student) * cnt;
 		idx++;
 	}
 	idx = 0;
-	while (idx < class_num) // 저장된 값을 %를 붙혀서 소수점 셋째 자리까지 출력
+	while (idx < class_num)
 	{
 		printf("%.3f%%\n", class[idx]);
 		idx++;
--- Baekjoon/func.c
+++ Baekjoon/func.c
@@ -1,12 +1,59 @@
-long long sum(int *a, int n)
-{
-	long long total = 0;
-	int	i = 0;
+#include	<stdio.h>
 
-	while (i < n)
+int	*selp_check(int *arr, int *check, int num)
+{
+	int	idx = 0, selp_idx = 0;
+	while (idx <= num)
 	{
-		total += a[i];
-		i++;
+		selp_idx = 0;
+		while (selp_idx <= num)
+		{
+			if (check[idx] == arr[selp_idx])
+				check[idx] = 0;
+			selp_idx++;
+		}
+		idx++;
 	}
-	return (total);
+	return (check);
+}
+
+void	selp_num(void)
+{
+	int	*answer;
+	int	arr[10000], selp_arr[10000];
+	int	num = 10000, idx = 1;
+
+	while (idx <= num)
+	{
+		selp_arr[idx] = idx;
+		idx++;
+	}
+	idx = 0;
+	while (idx <= num)
+	{
+		int	selp = 0;
+		if (9 >= idx)
+			selp = idx + idx;
+		else if (1000 <= idx)
+			selp = idx + (idx / 1000) + ((idx / 100) % 10) + ((idx / 10) % 10) + (idx % 10);
+		else if (100 <= idx)
+			selp = idx + (idx / 100) + ((idx / 10) % 10) + (idx % 10);
+		else if (10 <= idx)
+			selp = idx + (idx / 10) + (idx % 10);
+		arr[idx] = selp;
+		idx++;
+	}
+	answer = selp_check(arr, selp_arr, num);
+	idx = 0;
+	while (idx <= num)
+	{
+		if (0 < answer[idx])
+			printf("%d\n", answer[idx]);
+		idx++;
+	}
+}
+
+int main(void)
+{
+	selp_num();
 }
