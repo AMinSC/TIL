982b4c0086c4b23c9947d89af016ff0be0977688
AMinSC <https://github.com/AMiSC/TIL.git>
2023-02-20 00:09:14 +0900

    Solve Baekjoon With Python

--- Baekjoon/Python/step06/step06_1152.py
+++ Baekjoon/Python/step06/step06_1152.py
@@ -1,17 +1,1 @@
-def rev_ltoi (A:list):
-    a = []
-    for i in A:
-        a.append(i)
-    a.reverse()
-    f_n = int(a[0]) * 100
-    s_n = int(a[1]) * 10
-    t_n = int(a[2])
-    return f_n + s_n + t_n
-
-A, B = input().split()
-A = rev_ltoi(A)
-B = rev_ltoi(B)
-if A > B:
-    print(A)
-else:
-    print(B)
+print(len(input().split()))
