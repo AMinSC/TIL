c44da64db10cde7311ea0c5a613c0b1618649c59
민성철 <cdtjdcjf@gmail.com>
2023-03-03 21:56:42 +0900

    lru_cache

--- python_study/caching.py
+++ python_study/caching.py
@@ -1,9 +1,11 @@
+import sys
 from functools import lru_cache
 
+sys.setrecursionlimit(int(1e4))
 # 추가과제) 더 큰값을 계산하기 위해 StackOverflow한계치를 3000까지 늘려보세요!
 fib_arr = [0, 1]
 
-@lru_cache(maxsize=496)
+@lru_cache(maxsize=None) # maxsize 3과 동일하게 info가 나온다. 차이점은 캐시저장용량
 def fib_recur_dp(number: int) -> int:
 	if number < len(fib_arr):
 		return fib_arr[number]
@@ -13,3 +15,4 @@ def fib_recur_dp(number: int) -> int:
 		return fib
 # Hint : lru_cache 라는 기능이 있습니다.
 print(fib_recur_dp(3000))
+print(fib_recur_dp.cache_info())
