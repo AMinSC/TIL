9ba711cfe56c2d62e844ae2b282d591e549329aa
민성철 <cdtjdcjf@gmail.com>
2023-03-31 23:40:52 +0900

    making a chess game.

---
+++ python_study/Mini_games_for_learning_Python/chess.py
@@ -0,0 +1,38 @@
+"""make for chess game with python"""
+BOARD_SIZE = 8
+
+
+def board():
+    """체스 8X8 말판을 만든다."""
+
+
+def king():
+    """make for king"""
+
+
+def queen():
+    """make for queen"""
+
+
+def bishops():
+    """make for bishops"""
+
+
+def rooks():
+    """make for rooks"""
+
+
+def knights():
+    """make for knights"""
+
+
+def pawns():
+    """make for pawns"""
+
+
+def check_the_king():
+    """check the king"""
+
+
+def checkmate():
+    """checkmate"""
