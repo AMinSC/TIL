89063515e3cfc3b51932a9f5bd15772ec36779be
AMinSC <cdtjdcjf@gmail.com>
2023-04-04 23:29:52 +0900

    making a chess game.

--- python_study/Mini_games_for_learning_Python/chess.py
+++ python_study/Mini_games_for_learning_Python/chess.py
@@ -1,9 +1,13 @@
 """make for chess game with python"""
 BOARD_SIZE = 8
+ROWS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
 
 def board():
     """체스 8X8 말판을 만든다."""
+    chess_board = [row + str(val) for val in range(1, BOARD_SIZE + 1)
+                   for row in ROWS]
+    # print(chess_board)  # A1, B1, C1... F8, G8, H8
 
 
 def king():
