4f7cd44df0f0c9d7b9699ee1fd0832a3e9b2541b
AMinSC <cdtjdcjf@gmail.com>
2023-04-27 21:58:12 +0900

    solve baekjoon with python #25206

--- Baekjoon/Python/step06/step10_25206.py
+++ Baekjoon/Python/step06/step10_25206.py
@@ -1,10 +1,7 @@
-def rating(s: str):
-    check_rating = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F', 'P']
+def rating(s: str) -> int:
     rating_score = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0, 'C+': 2.5, 'C0': 2.0,
-                     'D+': 1.5, 'D0': 1.0, 'F': 0.0, 'P':0.0}
-    for i in check_rating:
-        if s == i:
-            return rating_score[i]
+                     'D+': 1.5, 'D0': 1.0, 'F': 0.0}
+    return rating_score[s]
 
 total_score = 0.0
 cnt = 0
@@ -12,11 +9,13 @@ cnt = 0
 while True:
     try:
         ans = list(map(str, input().split()))
-        ans_score = float(ans[1]) * rating(ans[2])
-        total_score += ans_score
-        cnt += float(ans[1])
+        if ans[2] != 'P':
+            total_score += float(ans[1]) * rating(ans[2])
+            cnt += float(ans[1])
     except:
+        if cnt == 0:
+            break
         total_score /= cnt
         break
 
-print(total_score)
+print('%.6f' % (total_score))
