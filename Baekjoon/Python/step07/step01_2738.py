33e6a465dff67158fbf81a643f217927f3be9334
AMinSC <cdtjdcjf@gmail.com>
2023-04-28 23:49:15 +0900

    EOFError #2738

--- Baekjoon/Python/step07/step01_2738.py
+++ Baekjoon/Python/step07/step01_2738.py
@@ -1,19 +1,17 @@
 N, M = map(int, input().split())
-
 ans = 0
 a_list = []
 b_list = []
 
 for _ in range(N):
-    num = map(int, input().split())
-    a_list.extend(num)
-
-for _ in range(N):
-    num = map(int, input().split())
-    b_list.extend(num)
+    num_arr = map(int, input().split())
+    a_list.extend(num_arr)
+for _ in range(M):
+    num_arr = map(int, input().split())
+    b_list.extend(num_arr)
 
 for i in range(N * M):
     a_list[i] += b_list[i]
     print(a_list[i], end=" ")
-    if (i+1) % 3 == 0:
+    if ((i+1) % N == 0) and (i+1 != (N * M)):
         print()
