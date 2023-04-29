7015ba76b8f0cdc52c4f78526c0a75af3d85300b
AMinSC <cdtjdcjf@gmail.com>
2023-04-29 22:34:07 +0900

    Solve Baekjoon With Python #2738

--- Baekjoon/Python/step07/step01_2738.py
+++ Baekjoon/Python/step07/step01_2738.py
@@ -6,12 +6,12 @@ b_list = []
 for _ in range(N):
     num_arr = map(int, input().split())
     a_list.extend(num_arr)
-for _ in range(M):
+for _ in range(N):
     num_arr = map(int, input().split())
     b_list.extend(num_arr)
 
 for i in range(N * M):
     a_list[i] += b_list[i]
     print(a_list[i], end=" ")
-    if ((i+1) % N == 0) and (i+1 != (N * M)):
+    if ((i+1) % M == 0) and (i+1 != (N * M)):
         print()
