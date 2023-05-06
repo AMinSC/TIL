9dd5c7e9425ef7f6d930716c4b7f6e6b551db84b
AMinSC <cdtjdcjf@gmail.com>
2023-05-06 23:44:53 +0900

    vector array count

--- Baekjoon/Python/step07/step04_2563.py
+++ Baekjoon/Python/step07/step04_2563.py
@@ -1,24 +1,11 @@
+board = [[0 for _ in range(101)] for _ in range(101)]
+
 cnt = int(input())
 
-total = cnt * 100
-board = []
-for i in range(cnt):
-    paper = list(map(int, input().split()))
-    board.append(paper)
+for _ in range(cnt):
+    x, y = map(int, input().split())
+    for row in range(x, x+10):
+        for col in range(y, y+10):
+            board[row][col] = 1
 
-a = list(sorted(board, key=lambda x: x[0], reverse=True))
-
-f_idx = 0
-s_idx = f_idx + 1
-while s_idx < len(board):
-    aa = a[f_idx][0] - a[s_idx][0]
-    if aa < 10:
-        row = 10 - aa
-        if a[f_idx][1] > a[s_idx][1]:
-            col = 10 - (a[f_idx][1] - a[s_idx][1])
-        else:
-            col = 10 - (a[s_idx][1] - a[f_idx][1])
-    f_idx += 1
-    s_idx += 1
-
-print(total - row * col)
+print(sum(val.count(1) for val in board))
