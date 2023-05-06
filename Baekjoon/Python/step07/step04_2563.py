90a65129f26672a942c486d7f53d7ac70d90e8c8
AMinSC <cdtjdcjf@gmail.com>
2023-05-06 23:26:58 +0900

    #2563 중복값 체크 풀이, 틀린 문제 추후 확인

--- Baekjoon/Python/step07/step04_2563.py
+++ Baekjoon/Python/step07/step04_2563.py
@@ -1,12 +1,24 @@
 cnt = int(input())
 
+total = cnt * 100
 board = []
-row = 0
-col = 0
 for i in range(cnt):
     paper = list(map(int, input().split()))
     board.append(paper)
 
-a = sorted(board, key=lambda x : x[0])
-b = list(zip(*a))
-print(f'board : {board}\na: {a}\nb: {b}\n')
+a = list(sorted(board, key=lambda x: x[0], reverse=True))
+
+f_idx = 0
+s_idx = f_idx + 1
+while s_idx < len(board):
+    aa = a[f_idx][0] - a[s_idx][0]
+    if aa < 10:
+        row = 10 - aa
+        if a[f_idx][1] > a[s_idx][1]:
+            col = 10 - (a[f_idx][1] - a[s_idx][1])
+        else:
+            col = 10 - (a[s_idx][1] - a[f_idx][1])
+    f_idx += 1
+    s_idx += 1
+
+print(total - row * col)
