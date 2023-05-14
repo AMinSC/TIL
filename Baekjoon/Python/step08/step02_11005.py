f60bd4ab17dda73a7159152b6aaa0c97ce4c8bab
AMinSC <cdtjdcjf@gmail.com>
2023-05-14 10:41:17 +0900

    진법 체크

--- Baekjoon/Python/step08/step02_11005.py
+++ Baekjoon/Python/step08/step02_11005.py
@@ -6,10 +6,20 @@ pure_dict = {val: str(val) for val in range(10)}
 num_dict.update(pure_dict)
 
 answer = ''
-while N >= B:
-    share, remainder = divmod(N, B)
+
+if N > 9:
+    while N >= B:
+        share, remainder = divmod(N, B)
+        answer += num_dict.get(remainder)
+        N = share
     answer += num_dict.get(remainder)
-    N = share
-answer += num_dict.get(remainder)
+else:
+    keep = []
+    while N >= B:
+        share, remainder = divmod(N, B)
+        keep.append(str(remainder))
+        N = share
+    answer += str(N)
+    answer.join(reversed(keep))
 
 print(answer)
