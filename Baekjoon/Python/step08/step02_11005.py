694d9a2543032cac1a96d96eb1683bdb88d17c44
AMinSC <cdtjdcjf@gmail.com>
2023-05-20 13:57:39 +0900

    Solve beakjoon with python #11005

--- Baekjoon/Python/step08/step02_11005.py
+++ Baekjoon/Python/step08/step02_11005.py
@@ -6,23 +6,14 @@ pure_dict = {val: str(val) for val in range(10)}
 num_dict.update(pure_dict)
 
 answer = ''
+keep = []
 
-if B > 9:
-    while N >= B:
-        share, remainder = divmod(N, B)
-        answer += num_dict.get(remainder)
-        N = share
-    answer += num_dict.get(remainder)
-else:
-    keep = []
-    while N >= B:
-        share, remainder = divmod(N, B)
-        keep.append(str(remainder))
-        N = share
-    keep.append(str(remainder))
-    if share//B > 0:
-        keep.append(str(share//B))
-    keep.reverse()
-    answer = answer.join(keep)
+while N >= B:
+    share, remainder = divmod(N, B)
+    keep.append(num_dict.get(remainder))
+    N = share
+keep.append(num_dict.get(N))
+keep.reverse()
+answer = answer.join(keep)
 
 print(answer)
