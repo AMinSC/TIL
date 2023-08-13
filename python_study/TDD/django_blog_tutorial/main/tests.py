649b8f8d0725e8afa712f54d9ba34a1efee70214
AMinSC <cdtjdcjf@gmail.com>
2023-08-13 21:17:45 +0900

    django TDD

---
+++ python_study/TDD/django_blog_tutorial/main/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+# class Test(TestCase):
+#     def test_something_one(self):
+#         self.assertEqual(True, False)
+#         '''
+#         블로그 리스트 체크
+#         '''
+
+#     def test_something_two(self):
+#         self.assertEqual(True, True)
+#         '''
+#         상속 체크
+#         '''
