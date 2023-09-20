545d7d7e767f0619dd2dee5f0ef6bcff8c4daa3a
AMinSC <cdtjdcjf@gmail.com>
2023-09-20 11:11:31 +0900

    #5073

--- Baekjoon/Python/step10/step07_5073.py
+++ Baekjoon/Python/step10/step07_5073.py
@@ -2,11 +2,11 @@ while True:
     a, b, c = map(int, input().split())
     if not (a + b + c):
         break
-    if 2 * max(a, b, c) >= sum((a, b, c)):
+    if a + b <= c:
         print("Invalid")
     elif a == b == c:
         print("Equilateral")
-    elif a == b | a == c | b == c:
+    elif a == b or a == c or b == c:
         print("Isosceles")
     else:
         print("Scalene")
