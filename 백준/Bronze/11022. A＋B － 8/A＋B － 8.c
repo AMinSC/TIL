5d116b0795c80ccc99c4819694ab2eb0f214e19a
Seong-Cheol, Min <99332204+AMinSC@users.noreply.github.com>
2023-10-19 23:32:52 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int t;
+    scanf("%d", &t);
+    int a[256], b[256];
+    
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d", &a[i], &b[i]);
+    }
+    
+    for (int i = 0; i < t; i++)
+    {
+        printf("Case #%d: %d + %d = %d\n", i + 1, a[i], b[i], a[i] + b[i]);
+    }
+    return 0;
+}
