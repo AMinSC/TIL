aef64cbaf9f4822d7f45f51789a04a1e993388c2
Seong-Cheol, Min <99332204+AMinSC@users.noreply.github.com>
2024-05-19 09:58:08 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,4 +1,4 @@
 def solution(data, ext, val_ext, sort_by):
-    exts = {"code": 0, "date": 1, "maximum": 2, "remain": 3}
-    filter_data = list(filter(lambda x: x[exts[ext]] < val_ext, data))
-    return sorted(filter_data, key=lambda x: x[exts[sort_by]])
+    exts = ("code", "date", "maximum", "remain")
+    filter_data = list(filter(lambda x: x[exts.index(ext)] < val_ext, data))
+    return sorted(filter_data, key=lambda x: x[exts.index(sort_by)])
