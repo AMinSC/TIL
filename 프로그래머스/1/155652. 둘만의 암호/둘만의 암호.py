39c91b2a6367efeb86c1646722b15663c939dd98
Seong-Cheol, Min <99332204+AMinSC@users.noreply.github.com>
2024-05-26 14:39:18 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -5,9 +5,7 @@ def solution(s, skip, index):
     for c in s:
         asc_len = len(asc_num)
         new_index = asc_num.index(ord(c)) + index
-        if new_index < asc_len:
-            answer += chr(asc_num[new_index])
-        else:
-            answer += chr(asc_num[(new_index) % asc_len])
         
+        answer += chr(asc_num[(new_index) % asc_len])
+    
     return answer
