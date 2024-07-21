cf563252beca9a7758d679d0602234f7e1865856
Seong-Cheol, Min <99332204+AMinSC@users.noreply.github.com>
2024-07-21 22:55:17 +0900

    [level 0] Title: 종이 자르기, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int M, int N) {
-        int answer = 0;
-        if (M == 1 && N == 1) {
-            return answer;
-        }
         
-        
-        return M * (N - 1) + (M - 1);
+        return M * N - 1;
     }
 }
