4c51c9a87e92f39cb234059697dcd078278c9d49
AMinSC <cdtjdcjf@gmail.com>
2024-08-06 09:52:48 +0900

    Django 관련 학습 폴더 정리

--- django-laerning/hands-on/log_scheduler/core/app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import Test
-
-urlpatterns = [
-    path('log/', Test.as_view()),
-]
--- django-laerning/hands-on/restapp-CBV/user/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-
-admin.site.register(User)  # admin 페이지에 등록
