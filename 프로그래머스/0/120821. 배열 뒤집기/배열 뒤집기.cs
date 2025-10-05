238a17c093cabb25b501888c3cfedb762c46c6d5
Seong-Cheol, Min <99332204+AMinSC@users.noreply.github.com>
2025-10-05 12:52:44 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.34 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -2,7 +2,6 @@ using System;
 
 public class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[] {};
         Array.Reverse(num_list);
         return num_list;
     }
