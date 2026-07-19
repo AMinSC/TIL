de8d346c00085778d24176a980e0c032b33f9519
Seong-Cheol, Min <99332204+AMinSC@users.noreply.github.com>
2026-07-19 23:33:56 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

--- C++/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
+++ C++/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int solution(int a, int b) {
-    int sum_a_b = std::stoi(std::to_string(a) + std::to_string(b));
-    int sum_b_a = std::stoi(std::to_string(b) + std::to_string(a));
+    int sum_a_b = stoi(to_string(a) + to_string(b));
+    int sum_b_a = stoi(to_string(b) + to_string(a));
     
     return sum_a_b > sum_b_a ? sum_a_b : sum_b_a;
 }
